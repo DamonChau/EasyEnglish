183fb38b9ca788c687cd09a3b0dfe4db5eda6a60
DamonChau <chauxtdu@outlook.com>
2023-03-28 13:30:29 +1000

    update api url

--- Projects/FrontEnd/easyenglish/src/features/helpers/contants.ts
+++ Projects/FrontEnd/easyenglish/src/features/helpers/contants.ts
@@ -13,7 +13,7 @@ export const messageDuplication = 'Duplicate date!!!'
 const prod = {
     url: {
         API_URL_FOLDER: '',
-        API_URL: 'https://gchweb.azurewebsites.net/'
+        API_URL: 'https://easyenglish.azurewebsites.net/'
     }
 };
 const dev = {
